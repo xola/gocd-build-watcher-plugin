d345b7b8af801d318cbe95af9dc39fff49267f50
Rushi Vishavadia <rushi.v@gmail.com>
2020-07-04 13:24:59 +0530

    Default to a non-general channel (temp for now is #rushi-test)

--- plugin/src/main/java/com/github/gmazzo/gocd/slack/SlackNotifier.java
+++ plugin/src/main/java/com/github/gmazzo/gocd/slack/SlackNotifier.java
@@ -39,7 +39,7 @@ public class SlackNotifier implements Notifier {
         String text = user != null ?
                 message.text.replaceAll("\\b" + Pattern.quote(userEmail) + "\\b", "<@" + user.getName() + ">") :
                 message.text;
-        String channel = this.channel != null ? this.channel : user != null ? "@" + user.getName() : "#general";
+        String channel = this.channel != null ? this.channel : user != null ? "@" + user.getName() : "#rushi-test";
 
         checkResponse(() -> slack.methods().chatPostMessage(ChatPostMessageRequest.builder()
                 .token(token)
