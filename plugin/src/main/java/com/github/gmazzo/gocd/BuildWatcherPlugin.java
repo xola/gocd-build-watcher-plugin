4f5fe2ce7cd755dcd01dcb088ca3b09d9af6f699
Rushi Vishavadia <rushi.v@gmail.com>
2020-07-28 09:24:59 +0530

    Bump version

--- plugin/src/main/java/com/github/gmazzo/gocd/BuildWatcherPlugin.java
+++ plugin/src/main/java/com/github/gmazzo/gocd/BuildWatcherPlugin.java
@@ -30,6 +30,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.Base64;
 
 import javax.xml.bind.DatatypeConverter;
 
